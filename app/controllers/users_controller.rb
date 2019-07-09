0c6b3c297ca27f1dd7cb8605a7540b272739f65a
phamdat <phamdat.ltd@gmail.com>
2019-07-09 11:24:33 +0700

    make logic for sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,37 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    check_password_confirmation
-    byebug
+    if check_password && check_email
+      @user.username = params[:user][:username]
+      @user.email = params[:user][:email]
+      @user.password = params[:user][:password]
+      @user.save
+      flash[:success] = 'Sign up successfully'
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
 private
 
-  def check_password_confirmation
-    if params[:user][:password] != params[:user][:password_confirmation]
+  def check_password
+    if params[:user][:password] != params[:user][:password_confirmation] || params[:user][:password].length < 6
       flash.now[:warning] = 'Something went wrong'
-      render :index
       return false
+    else
+      return true
     end
+  end
+  def check_email
+    if User.find_by_email(params[:user][:email])
+      flash.now[:warning] = 'Email exist'
+      return false
+    else
+      return true
+    end
+  end
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation,)
   end
 end
